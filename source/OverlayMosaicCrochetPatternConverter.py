12e01d96985f1d9c4d16a7a6ae92c6b43e6eed3e
SparkliTwizzl <github@sparklitwizzl.com>
2025-02-19 02:02:31 -0700

    refine OMC pattern converter, add test files and launch configs

--- source/OverlayMosaicCrochetPatternConverter.py
+++ source/OverlayMosaicCrochetPatternConverter.py
@@ -10,6 +10,11 @@
 import sys
 
 border_stitch = 'bs'
+row_color_right_padding = 10
+row_elements_number_color_pattern = 3
+row_elements_color_pattern = 2
+row_elements_pattern = 1
+row_number_left_padding = 5
 
 
 def main():
@@ -27,10 +32,10 @@ def main():
 
 
 def format_row_color(color):
-    return color + ' '
+    return color.rjust(row_color_right_padding) + ' '
 
 def format_row_number(row_number):
-    return str(row_number) + '. '
+    return str(row_number).ljust(row_number_left_padding) + '. '
 
 def format_row_pattern_segment(stitch_count, stitch_type):
     return str(stitch_count) + stitch_type + ', '
@@ -38,17 +43,33 @@ def format_row_pattern_segment(stitch_count, stitch_type):
 def format_starting_stitch(stitch_number):
     return '[' + str(stitch_number) + '] '
 
+def is_even(number):
+    return number % 2 == 0
+
+def is_odd(number):
+    return number % 2 == 1
+
 def load_input_file(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
-    return [line.strip().split(',') for line in lines]
+    return [line.strip().split('\t') for line in lines]
 
-def parse_row(row):
+def parse_row(row, row_number=None):
+    if len(row) == row_elements_number_color_pattern:
+        row_number = row[0]
+        row_color = row[1]
+        row_pattern = row[2]
+    elif len(row) == row_elements_color_pattern:
+        row_color = row[0]
+        row_pattern = row[1]
+    else:
+        row_color = 'A' if is_odd(int(row_number)) else 'B'
+        row_pattern = row[0]
     output = []
-    output.append(format_row_number(row[0]))
-    output.append(format_row_color(row[1]))
+    output.append(format_row_number(row_number))
+    output.append(format_row_color(row_color))
     output.append(border_stitch + ', ')
-    output.append(parse_row_pattern(row[2:]))
+    output.append(parse_row_pattern(row_pattern))
     output.append(border_stitch)
     return ''.join(output)
 
@@ -66,6 +87,11 @@ def parse_row_pattern(row_pattern):
 
 def parse_rows(rows):
     output = []
+    row_count = 1
     for row in rows:
-        output.append(parse_row(row))
+        if not row or len(row) < row_elements_pattern:
+            continue
+        row_number = row_count
+        output.append(parse_row(row, row_number))
+        ++row_count
     return output
